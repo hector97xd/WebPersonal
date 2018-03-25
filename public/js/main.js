7c42f4dd077ff55df71a435eacfce3cdb320ddca
hector97xd <37050929+hector97xd@users.noreply.github.com>
2018-03-24 21:13:30 -0600

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,38 @@
 var elem = document.querySelector('.sidenav');
 var edge  = 'left';
 var instance = M.Sidenav.init(elem, edge);
-instance.open();
-
 
 var elemCarousel = document.querySelector('.carousel');
 var carousel = M.Carousel.init(elemCarousel,{
     fullWidth: true
   });
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+      nombre: '',
+      email: '',
+      msj:'',
+      alert: false
+    },
+    methods:{
+      enviar: function(){
+        if (this.nombre && this.email && this.msj) {
+          alert("Enviado");
+          this.nombre = "";
+          this.email ="";
+          this.msj="";
+          this.alert = true;
+
+        } else {
+          alert("No enviado");
+        }
+      }
+    },
+    computed:{
+      validar:function(){
+        return this.nombre && this.email && this.msj;
+      }
+    }
+    
+  })
