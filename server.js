9928dd429b59a75351c6de9584431708ba52278d
hector97xd <37050929+hector97xd@users.noreply.github.com>
2018-04-02 08:48:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
+'use strict';
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var nodemailer = require('nodemailer');
 const path = require('path');
 
 app.set('port' , process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static(path.join(__dirname,'public')));
 
+// app.get('/index',function (req,res) {
+//     res.render('index',{qs: req.query});
+// })
+/*
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'hector.guevarah97@gmail.com',
+        pass: 'hectorMGH97'
+    }
+});
+const mailOptions = {
+    from: 'hector.guevarah97@gmail.com', // sender address
+    to: 'hector.guevarah97@gmail.com', // list of receivers
+    subject: 'MY CONTACT WEB', // Subject line
+    html: '<table><tr><td><label>Correo:</label></td><td><p>corre@gmail.com</p></td></tr><tr><td><label>Mensaje:</label></td><td><p>Contenido del mensaje</p></td></tr></table>'// plain text body
+
+};
+transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+        console.log(err)
+    else
+        console.log(info);
+});*/
 app.listen(app.get('port'), ()=>{
     console.log("server listening in port "+ app.get('port'));
 });
